d7b09b9b04a195b5f6b87bebdd02e3658ea1eb27
Qianqian Fang <fangqq@gmail.com>
2019-11-16 14:34:39 -0500

    Update example for bigint and buffer object

--- test/basictest.js
+++ test/basictest.js
@@ -3,16 +3,20 @@ var nj = require("numjs")
 var jd = require("../jdata.js")
 
 
-let a={integer:1,shortarray:[1,2,3],object:[[1,2,3],null,false]};
-a.a_typedarray=new Uint8ClampedArray([9,9,9,9]);
-a.a_ndarray=nj.arange(3*3,'int32').reshape(3,3);
-a.a_map=new Map([
-  [1, 'one'],
+let data={integer:1,shortarray:[1,2,3],object:[[[1],[2],[3]],null,false]};
+data.special=[NaN, Infinity, -Infinity,[],{}];
+data.a_typedarray=new Uint8ClampedArray([9,9,9,9]);
+data.a_ndarray=nj.arange(3*3,'int32').reshape(3,3);
+data.a_buffer=new ArrayBuffer(16);
+data.a_biginteger=9007199254740991n;
+data.a_bigintarray=BigUint64Array.from([9007199254740991n,347523475237452n]);
+data.a_map=new Map([
+  [0/0, 'one'],
   [2, 'two'],
   ["k", 'three'],
 ]);
 
-jd= new jdata(a,{compression:'zlib'});
+jd= new jdata(data,{compression:'zlib'});
 
 console.log(jd.tojson());
 console.log(jd.encode().tojson());
